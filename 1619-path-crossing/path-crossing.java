8745e204e537f8979934d01ff6e7b56437eb58da
Jaswanth <81699366+Jaswanth19-596@users.noreply.github.com>
2023-12-23 16:56:01 -0600

    Time: 11 ms (6.64%) | Memory: 42.3 MB (5.75%) - LeetSync

---
+++ 1619-path-crossing/path-crossing.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPathCrossing(String path) {
+        
+        int x = 0, y = 0;
+
+        HashSet<String> set = new HashSet<>();
+
+        set.add(x + " "+y);
+        for(char ch : path.toCharArray()){
+            switch(ch){
+                case 'N' : y = y + 1;  break;
+                case 'E' : x = x + 1; break;
+                case 'W' : x = x - 1; break;
+                case 'S' : y = y - 1; break;
+            }
+
+            System.out.println(x + " "+y);
+
+            if(set.contains(x+" "+y)){
+                return true;
+            }
+            set.add(x + " "+y);
+        }
+        return false;
+    }
+}
