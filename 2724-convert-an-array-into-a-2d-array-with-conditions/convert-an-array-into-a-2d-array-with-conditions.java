cacc126ca44e31163dc5936b4f7898e698fccc2d
Jaswanth <81699366+Jaswanth19-596@users.noreply.github.com>
2024-01-02 12:59:46 -0600

    Time: 6 ms (16.31%) | Memory: 45.4 MB (9.75%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,32 +1,38 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
         
-        List<Set<Integer>> res = new ArrayList<>();
-
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int max = 0;
 
         for(int ele : nums){
 
-            boolean inserted = false;
-
-            for(Set<Integer> set : res){
-                if(set.contains(ele) == false){
-                    set.add(ele);
-                    inserted = true;
-                    break;
-                }
+            if(map.containsKey(ele)){
+                int prev = map.get(ele);
+                prev++;
+                map.put(ele, prev);
+                max = Math.max(max, prev);
+            }
+            else{
+                map.put(ele, 1);
+                max = Math.max(max, 1);
             }
 
-            if(inserted == false){
-                Set<Integer> set = new HashSet<>();
-                set.add(ele);
-                res.add(set);
-            }
         }
-
         List<List<Integer>> list = new ArrayList<>();
 
-        for(Set<Integer> set : res){
-            list.add(new ArrayList<>(set));
+        for(int i = 0;i<max;i++){
+            list.add(new ArrayList<>());
+        }
+
+        for(Map.Entry<Integer, Integer> e : map.entrySet()){
+
+            int ele = e.getKey();
+            int val = e.getValue();
+
+
+            for(int i = 0;i<val;i++){
+                list.get(i).add(ele);
+            }
         }
 
         return list;
