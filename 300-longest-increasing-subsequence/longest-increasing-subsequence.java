7f2ef08c1f8448659e951c690994befe87437212
Jaswanth <81699366+Jaswanth19-596@users.noreply.github.com>
2024-02-11 14:19:56 -0600

    Time: 50 ms (24.81%) | Memory: 43.9 MB (75.96%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,33 +1,21 @@
 class Solution {
-
-    private int solve(int i, int prev, int []nums, int [][]dp){
-
-        if(i == nums.length)    return 0;
-
-        if(dp[i][prev+1] != -1) return dp[i][prev+1];
-
-        if(prev == -1 || nums[i] > nums[prev]){
-            // I have two options and I have to return the maximum of both answers
-            int pick = 1 + solve(i+1, i, nums, dp);
-            int notPick = 0 + solve(i + 1, prev, nums, dp);
-            dp[i][prev+1] = Math.max(pick, notPick);
-            // map.put(i+" "+prev, Math.max(pick, notPick));
-            return Math.max(pick, notPick);
-        }
-        else{
-            // You only have one option and you have to return that only option
-            int res =  solve(i+1, prev, nums, dp);
-            dp[i][prev+1] = res;
-            return res;
-        }
-    }
-
     public int lengthOfLIS(int[] nums) {
-        // Map<String, Integer> map = new HashMap<>();
-        int dp[][] = new int[nums.length + 1][nums.length + 1];
-        for(int arr[] : dp){
-            Arrays.fill(arr, -1);
+        
+        int dp[] = new int[nums.length];
+
+        Arrays.fill(dp, 1);
+
+        int max = 1;
+
+        for(int i = 0;i<nums.length;i++){
+            for(int j = 0;j<i;j++){
+                if(nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+                    max = Math.max(max, dp[i]);
+                }
+            }
         }
-        return solve(0, -1, nums, dp);
+        return max;
+
     }
 }
