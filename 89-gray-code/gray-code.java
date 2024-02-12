dd9ffc6c9ae1d304b86e2817cb3b9af0ad88447a
Jaswanth <81699366+Jaswanth19-596@users.noreply.github.com>
2024-02-12 13:36:14 -0600

    Time: 61 ms (8.28%) | Memory: 55.1 MB (18.61%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,48 +1,55 @@
 class Solution {
 
-    ArrayList<Integer> list = new ArrayList<>();
 
-
-    int getNum(int arr[]){
-        int n = 0;
-
-        for(int i = 0;i<arr.length;i++){
-            n += (arr[i] * Math.pow(2, arr.length - i - 1));
+    List<String> solve(int n){
+        
+        if(n == 1){
+            ArrayList<String> list = new ArrayList<>();
+            list.add("0");
+            list.add("1");
+            return list;
         }
 
-        return n;
+        List<String> prev = solve(n - 1);
+
+        List<String> curr = new ArrayList<>();
+
+        for(int i = 0;i<prev.size();i++){
+            String appended = '0' + String.valueOf(prev.get(i));
+            // int m = Integer.parseInt(appended);
+            curr.add(appended);
+        }
+        for(int i = prev.size()-1;i>=0;i--){
+            String appended = '1' + String.valueOf(prev.get(i));
+            // int m = Integer.parseInt(appended);
+            curr.add(appended);
+        }
+        return curr;
+
     }
 
+    int getNum(String str){
 
-    boolean solve(int arr[], Set<Integer> set){
+        int res = 0;
 
-        if(list.size() == Math.pow(2, arr.length)){
-            return true;
+        for( int i = 0;i<str.length();i++){
+            char ch = str.charAt(i);
+            int bit = ch - '0';
+            res = res + (int)(bit * Math.pow(2, str.length() - i - 1));
         }
-
-        int n = getNum(arr);
-
-        if(set.contains(n)) return false;
-
-        set.add(n);
-        list.add(n);
-
-        for(int i = 0;i<arr.length;i++){
-            arr[i] = arr[i] ^ 1;
-            boolean res = solve(arr, set);
-            if(res == true) return true;
-            arr[i] = arr[i] ^ 1;
-        }
-        list.remove(list.size()-1);
-        set.remove(n);
-
-        return false;
+        return res;
     }
 
 
     public List<Integer> grayCode(int n) {
-        int arr[] = new int[n];
-        solve(arr, new HashSet<>());
-        return list;
+        List<String> res =  solve(n);
+        List<Integer> finalArr = new ArrayList<>();
+
+        for(String str : res){
+            finalArr.add(getNum(str));
+        }
+        return finalArr;
+
+
     }
 }
