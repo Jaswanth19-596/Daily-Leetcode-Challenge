fe85361f4f996687b43255b345d54d07f836b16c
Jaswanth <81699366+Jaswanth19-596@users.noreply.github.com>
2024-02-16 23:52:06 -0600

    Time: 19 ms (69.61%) | Memory: 57 MB (90.09%) - LeetSync

--- 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.java
+++ 1762-furthest-building-you-can-reach/furthest-building-you-can-reach.java
@@ -1,45 +1,25 @@
 class Solution {
 
-    int solve(int i, int bricks, int ladders, int heights[], HashMap<String, Integer> map){
-
-        if(bricks < 0 || ladders < 0)   return 0;
-
-        if(i == heights.length - 1) return 1;
-        String key = i + " "+bricks + " "+ladders;
-        if(map.containsKey(key))    return map.get(key);
-
-        if(heights[i+1] < heights[i])   return 1+solve(i+1, bricks, ladders, heights,map);
-
-        int tookBricks = solve(i+1, bricks - (heights[i+1] - heights[i]), ladders, heights, map);
-
-        int tookLadder = solve(i+1, bricks, ladders - 1, heights, map);
-
-        int res =  1 + Math.max(tookBricks, tookLadder);
-
-        map.put(key, res);
-
-        return res;
-
-    }
-    public int furthestBuilding(int[] heights, int bricks, int ladders) {
-        int count = 1;
+     public int furthestBuilding(int[] heights, int bricks, int ladders) {
+        int count = 0;
 
         PriorityQueue<Integer> pq = new PriorityQueue<>();
 
         for(int i = 1;i<heights.length;i++){
             if(heights[i] - heights[i-1] > 0){
+                // We are using a ladder
                 pq.add(heights[i] - heights[i-1]);
             }
 
             if(pq.size() > ladders){
                 int diff = pq.poll();
-                if(bricks < diff)   return count-1;
+                if(bricks < diff)   return count;
                 bricks -= diff;
             }
             count++;
         }
 
-        return count-1;
+        return count;
 
     }
 }
