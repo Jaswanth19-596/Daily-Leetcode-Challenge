b880977915696d39c7a56dc5da8c680bd9434f1f
Jaswanth <81699366+Jaswanth19-596@users.noreply.github.com>
2024-03-04 00:00:10 -0600

    Time: 2 ms (97.13%) | Memory: 42.7 MB (57.01%) - LeetSync

--- 985-bag-of-tokens/bag-of-tokens.java
+++ 985-bag-of-tokens/bag-of-tokens.java
@@ -1,25 +1,29 @@
 class Solution {
-    public int bagOfTokensScore(int[] tokens, int power) {
-        int maxScore = 0;
+    public int bagOfTokensScore(int[] token, int power) {
         int score = 0;
-        int i = 0, j = tokens.length - 1;
-        Arrays.sort(tokens);
+        int i = 0, j = token.length - 1;
+        int maxScore = 0;
+
+        Arrays.sort(token);
+
         while(i<=j){
-            while(i<=j && tokens[i] <= power){
-                power = power - tokens[i];
-                score++;
+
+            // I will always try to play it as A
+            while(i<=j && token[i] <= power){
+                power = power - token[i];
+                score += 1;
                 maxScore = Math.max(score, maxScore);
                 i++;
             }
-            if(score < 1) return maxScore;
 
-            power = power + tokens[j];
+            // I will try to play it as B
+            if(score < 1)   return maxScore;
+
+            power = power + token[j];
             score = score - 1;
             j--;
         }
 
         return maxScore;
-
-
     }
 }
