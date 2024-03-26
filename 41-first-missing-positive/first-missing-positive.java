d18133cab327fa9de9e919e3cbef1a8a4d845a5a
Jaswanth <81699366+Jaswanth19-596@users.noreply.github.com>
2024-03-25 22:18:27 -0500

    Time: 1 ms (98.00%) | Memory: 55 MB (61.84%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,31 +1,39 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        
+        // for(int i = 0;i<nums.length;i++){
+            
+        // }
 
         int i = 0;
 
         while(i < nums.length){
-            if(nums[i] <=0 || nums[i] > nums.length){
+            if(nums[i] > nums.length || nums[i] <=0){
                 i++;
                 continue;
             }
 
-            if(nums[i] == nums[nums[i] - 1]){
+            if(nums[i] == i+1){
+                i++;
+                continue;
+            }
+
+            if(nums[i] == nums[nums[i]-1]){
                 i++;
                 continue;
             }
 
             int temp = nums[i];
-            nums[i] = nums[temp - 1];
-            nums[temp - 1] = temp;
-            // i++;
+            nums[i] = nums[temp-1];
+            nums[temp-1] = temp;
         }
-        System.out.println(Arrays.toString(nums));
-        for(i = 0 ;i< nums.length;i++){
+
+        for(i = 0;i<nums.length;i++){
             if(nums[i] != i+1){
                 return i+1;
             }
         }
-        return nums.length + 1;
+        return nums.length+1;
 
     }
 }
