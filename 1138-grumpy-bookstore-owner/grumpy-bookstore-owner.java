bb15b6fceee4963a77d8edd28d5f6e9b3e0255ce
Jaswanth <81699366+Jaswanth19-596@users.noreply.github.com>
2024-06-20 22:25:50 -0500

    Time: 3 ms (91.49%) | Memory: 45.6 MB (96.53%) - LeetSync

--- 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
+++ 1138-grumpy-bookstore-owner/grumpy-bookstore-owner.java
@@ -1,42 +1,50 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
         
+
         int initialSatisfaction = 0;
 
         for(int i = 0;i<customers.length;i++){
-            if(grumpy[i] != 1){
+            if(grumpy[i] !=1 ){
                 initialSatisfaction += customers[i];
             }
         }
 
-        int low = 0, high = 0;
         int windowSatisfaction = 0;
+
+        int low = 0, high = 0;
+
         while(high < minutes){
+            
+            if(grumpy[high] == 1){
+                windowSatisfaction += customers[high];
+            }
+
+            high++;
+        }
+
+
+        int totalCustomersSatisfied = initialSatisfaction + windowSatisfaction;
+
+        while(high < customers.length){
 
             if(grumpy[high] == 1){
                 windowSatisfaction += customers[high];
             }
             high++;
-        }
-
-        int maxSatisfaction = initialSatisfaction + windowSatisfaction;
-        while(high < customers.length){
 
             if(grumpy[low] == 1){
                 windowSatisfaction -= customers[low];
             }
-            low++;
+            low++ ;
 
-            if(grumpy[high] == 1){
-                windowSatisfaction += customers[high];
-            }
-            high++;
-
-            maxSatisfaction = Math.max(initialSatisfaction + windowSatisfaction, maxSatisfaction);
-
+            totalCustomersSatisfied = Math.max(totalCustomersSatisfied, initialSatisfaction + windowSatisfaction);
         }
 
-        return maxSatisfaction;
+        return totalCustomersSatisfied;
+
+
+
 
 
     }
