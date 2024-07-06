4a8d44fc2819024f171e5b929c24f0a0d2ad28b1
Jaswanth <81699366+Jaswanth19-596@users.noreply.github.com>
2024-07-05 20:03:05 -0500

    Time: 0 ms (100.00%) | Memory: 40.6 MB (16.67%) - LeetSync

--- 2645-pass-the-pillow/pass-the-pillow.java
+++ 2645-pass-the-pillow/pass-the-pillow.java
@@ -1,32 +1,28 @@
 class Solution {
     public int passThePillow(int n, int time) {
        
-        int direction = 1;
+       int i = 1, direction = 1;
 
-        int arr[] = new int[n+1];
+       while(time > 0){
 
-        int i = 1;
-
-        while(time > 0){
-
-            if(direction == 1){
-                i++;
-            }
-            else{
-                i--;
-            }
-
-            if(i == n){
-                direction = -1;
-            }
-            if(i == 1){
-                direction = 1;
-            }
-            time--;
+        if(direction == 1){
+            i++;
+        }
+        else{
+            i--;
         }
 
-        return i;
+        if(i == n){
+            direction = -1;
+        }
+        if(i == 1){
+            direction = 1;
+        }
 
+        time--;
+       }
+
+       return i;
 
     }
 }
