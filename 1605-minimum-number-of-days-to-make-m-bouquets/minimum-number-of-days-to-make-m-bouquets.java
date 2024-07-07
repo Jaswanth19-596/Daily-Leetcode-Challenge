34d72e2a837f04b36a5dc923dfa4e0ccff52465a
Jaswanth <81699366+Jaswanth19-596@users.noreply.github.com>
2024-07-07 00:18:34 -0500

    Time: 23 ms (32.80%) | Memory: 58.3 MB (34.06%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,52 +1,63 @@
 class Solution {
 
+    public boolean canIDoBouquets(int []bloomDay, int mid, int m, int k){
 
-    public int minDays(int[] bloomDay, int m, int k) {
-        
+        int nBouquets = m;
 
-        // I'll APply binary search on the bloomday.
-        int min = 1;
-        int max = (int)1e9;
+        int i = 0;
 
-        int res = -1;
+        while(i < bloomDay.length){
 
-        while(min <= max){
-
-            int mid = min + (max - min)/2;
-
-            int currentDay = mid;
-
-            int bouques = 0;
             int flowers = 0;
+            while(i<bloomDay.length){
 
-// 50000
-            int count = 0;
-
-            for(int i = 0;i<bloomDay.length;i++){
-                if(bloomDay[i] <= currentDay){
+                if(bloomDay[i] <= mid){
                     flowers++;
-                    
-                    if(flowers == k){
-                        bouques++;
-                        flowers = 0;
-                    }
                 }
                 else{
-                    flowers=0;
+                    flowers = 0;
                 }
-            }
-
-            if(bouques >= m){
-                res = currentDay;
-                max = currentDay - 1;
-            }
-            else{
-                min = currentDay + 1;
-            }
 
 
+                if(flowers == k){
+                    nBouquets--;
+                    flowers = 0;
+                }
+                i++;
+            }
         }
 
-        return res;
+        if(nBouquets <= 0)  return true;
+        return false;
+    }
+
+
+    public int minDays(int[] bloomDay, int m, int k) {
+
+        if( m * k > bloomDay.length)    return -1;
+
+        // 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 
+        // If you can do the bouquets in nth day, then you can do it in N -> Inf days. Since we need minimum no of days, there's no point in going to next days.
+        // We will store this answer as a temporary answer and try to do it in even more minimum no of days
+        
+        // If we cannot do the required bouquets on some xth day, that means, we cannot do it in x - 1 -> -INF.
+
+        int low = 1, high = (int)1e9;
+
+        int ans = -1;
+
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            boolean res = canIDoBouquets(bloomDay, mid, m, k);
+            if(res){
+                ans = mid;
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+
+        return ans;
     }
 }
