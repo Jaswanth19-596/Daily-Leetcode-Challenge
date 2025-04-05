783691eb41a101dbda05e0a4911b7e3fd9d4232a
Jaswanth <81699366+Jaswanth19-596@users.noreply.github.com>
2025-04-05 12:23:54 -0500

    Time: 11 ms (48.29%) | Memory: 17.9 MB (69.91%) - LeetSync

--- 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
+++ 3522-find-the-power-of-k-size-subarrays-i/find-the-power-of-k-size-subarrays-i.py
@@ -1,25 +1,46 @@
 class Solution:
 
-    def checkIfSorted(self, start, end, nums):
-        for i in range(start + 1, end+1):
 
-            if nums[i] - nums[i-1] != 1:
-                return False
+    def fill(self,res, start , k):
+        # if start >= len(res):
+        #     return
+        stop = start - (k - 1)
+        while start >= 0 and start > stop:
+            if start < len(res):
+                res[start] = -1
+            start -= 1
 
-        return True
+        
+
 
     def resultsArray(self, nums: List[int], k: int) -> List[int]:
         
-        res = []
+        # Go through the array and find the mismatches:
+            # If there is a mismatch:
+                # Start from the first element and go backwards till k-1 elements
+                # Fill all of these with -1
 
+            # else
+                # Move to the next element
+
+        res = [0] * (len(nums) - k + 1)
+
+        if k == 1 and len(nums) == 1:
+            return nums
+        if k == 1:
+            return nums
+
+        for i in range(len(nums)-1):
+
+            if nums[i] - nums[i+1] != -1:
+
+                self.fill(res, i, k)
+                
         for i in range(len(nums) - k + 1):
-            
-            isSorted = self.checkIfSorted(i, i + k-1, nums)
 
-            if isSorted == False:
-                res.append(-1)
-            else:
-                res.append(nums[i+k-1])
+            if res[i] == 0:
+                res[i] = nums[i + k - 1]
 
         return res
+
         
